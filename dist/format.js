4babb853fed76caf36c796be14f11610f8bb0099
Elkie Nova <autumn.leaves.on.jupiter@gmail.com>
2019-08-06 00:30:32 +0300

    configured npm watch

--- dist/format.js
+++ dist/format.js
@@ -3,9 +3,9 @@ window.storyFormat({
 	"version":"1.0.0",
 	"author":'<a href="https://twitter.com/aloelazoe">Elkie Nova</a>',
 	"description":"a cute little story format",
-	"image":"icon.svg",
+	// "image":"icon.svg",
 	"url":"not yet",
 	"license":"MIT License",
 	"proofing":false,
-	"source":"<head><title>{{STORY_NAME}}</title><meta charset=utf-8></head><body><div id=data>{{STORY_DATA}}</div><script>console.log(\"hiya i'm a cute little story format!!! it's really me!!!\"),console.log(\"and i have a lot more to say ✨💜✨[*^ ‿ ^*]✨💜✨\"),console.log(\"he he he\");</script></body>",
+	"source":"<head><title>{{STORY_NAME}}</title><meta charset=utf-8></head><body><div id=data>{{STORY_DATA}}</div><script>console.log(\"hiya i'm a cute little story format!!! it's really me!!!\"),console.log(\"and i have a lot more to say ✨💜✨[*^ ‿ ^*]✨💜✨\"),console.log(\"he he he\"),console.log(\"and another one!!!\");</script></body>",
 });
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
 console.log("hiya i'm a cute little story format!!! it's really me!!!");
 console.log("and i have a lot more to say ✨💜✨[*^ ‿ ^*]✨💜✨");
 console.log("he he he");
+console.log("and another one!!!");
